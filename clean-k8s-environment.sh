a94e5a02dea2380c80d9e1f045b72c2515d1e399
Murtaza Rangwala <murtazahatimr@icloud.com>
2025-07-22 15:35:26 +1000

    Remove wrong statement from cleanup script.

--- clean-k8s-environment.sh
+++ clean-k8s-environment.sh
@@ -3,7 +3,6 @@
 # Cleanup kubernetes environment if it exists.
 kubectl delete -f kubernetes-manifests/generated/redis-cluster.yaml
 kubectl delete -f kubernetes-manifests/generated/blockchain-network-deployment.yaml
-kubectl delete -f kubernetes-manifests/generated/federated-schedule-tp-deployment.yaml
 kubectl delete -f kubernetes-manifests/static/local-docker-registry-deployment.yaml
 kubectl delete -f kubernetes-manifests/generated/couchdb-cluster-deployment.yaml
 kubectl delete -f kubernetes-manifests/generated/config-and-secrets.yaml
